489b2e9d4e71f4a4e3c01104ef14455d94b95c62
BorisovKostya2006 <kostyaBorisov075@gmail.com>
2025-12-16 22:23:26 +0300

    added NavGraph

--- app/src/main/java/com/example/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notes/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.notes.presentation.navigation.NavGraph
 import com.example.notes.presentation.screens.creation.CreateNoteScreen
 import com.example.notes.presentation.screens.edit.EditNoteScreen
 import com.example.notes.presentation.screens.notes.NotesScreen
@@ -17,25 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NotesTheme {
                 Log.d("empty screen", "MainActivity")
-                EditNoteScreen(
-                    noteId = 5,
-                    onFinished = {
-                      Log.d("MainActivity","onFinished")
-                   }
-                )
-//                CreateNoteScreen(
-//                    onFinished = {
-//                        Log.d("MainActivity","onFinished")
-//                    }
-//                )
-//                NotesScreen(
-//                    onNoteClick = {
-//                        Log.d("MainActivity","onNoteClick")
-//                    },
-//                    onAddNoteClick = {
-//                        Log.d("MainActivity","onAddNoteClick")
-//                    }
-//                )
+                NavGraph()
             }
 
         }
---
+++ app/src/main/java/com/example/notes/presentation/navigation/NavGraph.kt
@@ -0,0 +1,61 @@
+package com.example.notes.presentation.navigation
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.notes.presentation.screens.creation.CreateNoteScreen
+import com.example.notes.presentation.screens.edit.EditNoteScreen
+import com.example.notes.presentation.screens.notes.NotesScreen
+
+@Composable
+fun NavGraph (){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Notes.route
+    ){
+        composable(Screen.Notes.route) {
+            NotesScreen(
+                onNoteClick = {
+                    Log.d("MainActivity","onNoteClick")
+                    navController.navigate(Screen.EditNote.route)
+                },
+                onAddNoteClick = {
+                    Log.d("MainActivity","onAddNoteClick")
+                    navController.navigate(Screen.CreateNote.route)
+                }
+            )
+        }
+        composable(Screen.EditNote.route) {
+            EditNoteScreen(
+                noteId = 5,
+                onFinished = {
+                    Log.d("MainActivity","onFinished")
+                    navController.popBackStack()
+                }
+            )
+        }
+        composable(Screen.CreateNote.route) {
+
+            CreateNoteScreen(
+                onFinished = {
+                    Log.d("MainActivity","onFinished")
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+
+
+
+}
+
+sealed class Screen(val route : String){
+    data object Notes : Screen(route = "notes")
+    data object EditNote : Screen(route = "EditNote")
+    data object CreateNote : Screen(route = "CreateNote")
+
+
+}
